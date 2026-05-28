e5bf72b32780334fa6d3862632b52b763088f7f1
moviesound <moviesound@ya.ru>
2026-05-28 23:09:53 +0400

    dz-11

--- app/routes/admin/logs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-//Route::get('/logs', [ActionLogController::class, 'index'])
-//    ->name('logs.index')
-//    ->middleware('permission:logs.view');
